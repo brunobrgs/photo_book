74878b0c7235834a615fae384d208dec6a59fa36
Bruno Borges <bruno.rails@gmail.com>
2015-04-30 23:14:16 -0300

    Carregar JS do box das imagens em a cada requisição

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,6 +5,7 @@
     url: 'load_more?page=' + page,
     complete: function() {
       if ($('.thumbnails').data('max') > page) {
+        $.getScript( "/assets/photographer.js", function() {});
         setTimeout(worker(page + 1), 5000);
       } else {
         $.getScript( "/assets/photographer.js", function() {});
